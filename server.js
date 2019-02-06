774ee02718f83bce85ba3f6d68e6e66223d70c62
dkwok94 <dhklangs@gmail.com>
2019-02-06 00:18:26 -0800

    Remove database reference from root route

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('app running');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
