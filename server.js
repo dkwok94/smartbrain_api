d3f04fa85329f2fe5809282eee4c436106adafbb
dkwok94 <dhklangs@gmail.com>
2019-02-06 00:33:52 -0800

    Add database URL environment variable

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : 'postgresql-round-69180',
-    user : 'postgres',
-    password : 'password',
-    database : 'smartbrain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
